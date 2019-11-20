993a98d40078a2df0a7440c8b2f85bc36e0121b6
An Lam <anlam12787@gmail.com>
2019-11-20 15:42:34 +0700

    Add example for using compute

--- lib/screens/tab1.dart
+++ lib/screens/tab1.dart
@@ -1,8 +1,8 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:isolate_worker/isolate_tasks/get_todo_task.dart';
 import 'package:isolate_worker/worker/worker.dart';
 
-import 'package:isolate_worker/isolate_tasks/get_todo_task.dart';
 import '../todo_model.dart';
 
 class Tab1 extends StatefulWidget {
@@ -49,8 +49,15 @@ class _Tab1State extends State<Tab1> {
     var todoTask = GetTodoTask(dio: dio, id: 1);
     final worker = Worker(poolSize: 2);
     todoModel = (await worker.handle(todoTask)) as TodoModel;
+
+//    todoModel = await compute(handle, todoTask);
+
     setState(() {
       loading = false;
     });
+  }
+
+  static Future<TodoModel> handle(GetTodoTask task) async {
+    return task.execute();
   }
 }
